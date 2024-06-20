7c6df31b700336a569db847fb980b0c8e1090141
vestruhhh <matthewnicol07@gmail.com>
2024-06-20 16:13:56 -0400

    Completed general functionality for searching

--- js/script.js
+++ js/script.js
@@ -1,26 +1,80 @@
+// const apiKey = "40809c32279c3fc1b8a5a1b9b3d978ab";
+// const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+// const searchBox = document.querySelector(".search input");
+// const searchButton = document.querySelector(".search button");
+
+// async function checkWeather() {
+//     const response = await fetch(apiUrl + `&appid=${apiKey}`);
+//     var data = await response.json();
+
+//     console.log(data)
+
+//     document.querySelector(".city").innerHTML = data.name;
+//     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+//     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+//     document.querySelector(".wind").innerHTML = data.wind.speed + " mph";
+
+
+// }
+
+// searchButton.addEventListener("click", () => {
+//     checkWeather(searchBox.value);
+// })
+
 const apiKey = "40809c32279c3fc1b8a5a1b9b3d978ab";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchButton = document.querySelector(".search button");
 
-async function checkWeather(city) {
-    // Fetchs URL to  check weather in particular city 
-    const response = await fetch(apiUrl + city + '&appid=${apiKey}');
-    var data = await  response.json();
+const weatherIcon = document.querySelector(".weather-icon")
 
-    console.log(data);
+const cityElement = document.querySelector(".city");
+const tempElement = document.querySelector(".temp");
+const humidityElement = document.querySelector(".humidity");
+const windElement = document.querySelector(".wind");
 
-    document.querySelector(".city").innerHTML = data.name;
-    // Assuming this turns out to celsius once the API Key loads convert to fahrenheit
-    document.querySelecter(".temp").innerHTML = Math.round(data.main.temp) + "°F";
-    document.querySelecter(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelecter(".wind").innerHTML = data.main.wind.speed + "mph";
+async function checkWeather(cityName) {
+    try {
+        const response = await fetch(apiUrl + `${cityName}&appid=${apiKey}`);
+        const data = await response.json();
 
+        // console.log(data);
+
+        cityElement.innerHTML = data.name;
+        tempElement.innerHTML = Math.round(data.main.temp) + "°F";
+        humidityElement.innerHTML = data.main.humidity + "%";
+        windElement.innerHTML = data.wind.speed + " mph";
+
+        if(data.weather[0].main == "Clouds") {
+            weatherIcon.src = "../images/clouds.png"
+        }
+        else if(data.weather[0].main == "Clear") {
+            weatherIcon.src = "../images/clear.png"
+        }
+        else if(data.weather[0].main == "Rain") {
+            weatherIcon.src = "../images/rain.png"
+        }
+        else if(data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "../images/drizzle.png"
+        }
+        else if(data.weather[0].main == "Mist") {
+            weatherIcon.src = "../images/mist.png"
+        }
+
+        document.querySelector(".weather").style.display = "block";
+
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
 }
 
-searchButton.addEventListener("click", ()=> {
-    // This will grab the city name from the field and pass it the function. 
-    // i.e. I want to see the weather in Gothenburg, Sweden. Gothenburg is passed to checkWeather
-    checkWeather(searchBox.value);
-})
+searchButton.addEventListener("click", () => {
+    const cityName = searchBox.value.trim();
+    if (cityName) {
+        checkWeather(cityName);
+    } else {
+        alert("Please enter a city name.");
+    }
+});
